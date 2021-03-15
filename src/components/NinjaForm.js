6bad21760e69c935e6b194fa6cbc9d1571cdc743
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-03-15 12:28:34 -0400

    first commit

---
+++ src/components/NinjaForm.js
@@ -0,0 +1,78 @@
+import React , {useState} from 'react';
+// import styles from './MyButtonComponent.module.css';
+import styles from './NinjaForm.module.css';
+
+
+const NinjaForm = () => {
+   
+
+
+    const [formInfo, setFormInfo] = useState({
+        ninjaName: "",
+        ninjaImg: "",
+        ninjaLang: "",
+        ninjaFavColor: ""
+    })
+    const [allNinjas, setAllNinjas ] = useState([])
+
+    const changeHandler = (e)=>{
+        setFormInfo({
+            ...formInfo,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const submitHandler = (e)=>{
+        e.preventDefault()
+        // allNinjas.push(formInfo) CANT DO IT THIS WAY IN REACT
+        setAllNinjas([...allNinjas, formInfo])
+        setFormInfo({
+            ninjaName: "",
+            ninjaImg: "",
+            ninjaLang: "",
+            ninjaFavColor: ""
+        })
+       
+        
+    }
+
+    return (
+        <>
+        <div>
+             <form onSubmit = {submitHandler} className = "col-6 mx-auto">
+                <div className="form-group">
+                    <label htmlFor="">Ninja Name</label>
+                    <input className="form-control" type="text" name="ninjaName" id="" onChange= {changeHandler} value = {formInfo.ninjaName}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">Image Link</label>
+                    <input className="form-control" type="text" name="ninjaImg" id="" onChange= {changeHandler} value = {formInfo.ninjaImg} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">Favorite Language</label>
+                    <input className="form-control" type="text" name="ninjaLang" id="" onChange= {changeHandler} value = {formInfo.ninjaLang}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">Favorite Color</label>
+                    <input className="form-control" type="text" name="ninjaFavColor" id="" onChange= {changeHandler} value = {formInfo.ninjaFavColor}/>
+                </div>
+               
+                <button type="submit" className="btn btn-success">Add Ninja</button>
+
+            </form>
+        </div>
+        <div className = {styles.container}>
+            {allNinjas.map(ninja =>{
+                return <div className= {styles.individualNinja} style = {{backgroundColor: ninja.ninjaFavColor }}>
+                    <p>Ninja Name: {ninja.ninjaName}</p>
+                    <p>Favorite Language: {ninja.ninjaLang}</p>
+                    <p><img src={ninja.ninjaImg} alt="" height= "200px" width= "200px"/></p>
+                </div>
+            })}
+        </div>
+        </>
+    );
+};
+
+
+export default NinjaForm;
